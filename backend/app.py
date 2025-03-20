0ad737fc02719a5a69024e8b457ada7e169f0336
Rashid <ashishalrashid@gmail.com>
2025-03-20 17:41:52 +0530

    dbms schema in backend

---
+++ backend/app.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_restful import Api, Resource
+from datetime import timedelta
+from flask_cors import cross_origin
+
+
+app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type', 'Authorization'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+app.config['JWT_SECRET_KEY']='super-secret'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
+
+db = SQLAlchemy(app)
+api=Api(app)
+CORS(app)
+jwt =JWTManager(app)
+
+#make this intosperate file models.py in the future mabye?
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta, date
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), nullable=False) 
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    full_name = db.Column(db.String(128), nullable=False)  
+    qualification = db.Column(db.String(32), nullable=False) 
+    dob = db.Column(db.Date, nullable=True) 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False) 
+    start_date = db.Column(db.Date, nullable=True)
+    end_date = db.Column(db.Date, nullable=True)  # Deadline
+    time_duration = db.Column(db.Interval, nullable=False, default=timedelta(minutes=20))
+
+class Scores(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+    score = db.Column(db.Integer)  
+
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(), nullable=False)
+    option1 = db.Column(db.String())
+    option2 = db.Column(db.String())
+    option3 = db.Column(db.String())
+    option4 = db.Column(db.String())
+    answer = db.Column(db.Integer, nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False) 
+
+class User_Subject(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)  
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+
+class Question_Quiz(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)  
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False) 
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False) 
